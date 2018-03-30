99dbd755dc39a7b29792022a7c79bfaeeeeae86e
NikolaiNekrasov <nikolai.nekrasov@gmail.com>
2018-03-30 11:38:05 +0400

    Small text edit in tamplates

--- views/articles.php
+++ views/articles.php
@@ -2,15 +2,15 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Мой первый блог</title>
+    <title>My first blog</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  
 
   </head>
   <body>
     <div class="container">
-      <h1>Мой первый блог</h1>
-      <a href="admin">Панель Администратора</a>
+      <h1>My first blog</h1>
+      <a href="admin">Admin Panel</a>
       <div class="">
         <?php foreach($articles as $a): ?>
         <div class="article">
@@ -18,13 +18,13 @@
             <a href="article.php?id=<?=$a['id']?>">
               <?=$a['title']?></a>
           </h3>
-          <em>Опубликовано: <?=$a['date']?></em>
+          <em>Published: <?=$a['date']?></em>
           <p><?=articles_intro($a['content'])?></p>
         </div>
       <?php endforeach ?>
       </div>
       <footer>
-        <p>Мой Первый Блог <br>Copyright &copy; 2015</p>
+        <p>My first blog <br>Copyright &copy; 2018</p>
       </footer>
     </div>
   </body>
