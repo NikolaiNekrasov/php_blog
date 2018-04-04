46f8b74039498c9ca65e3fc49e3c60046f4704cb
Nick <nikolai.nekrasov@gmail.com>
2018-04-04 15:30:42 +0300

    add navbar to top

--- index.php
+++ index.php
@@ -14,8 +14,24 @@
 <div class="container-fluid">
     <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-        <span class="navbar-toggler-icon"></span>        
+        <span class="navbar-toggler-icon"></span>             
     </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+        </ul>
+    </div>
 </div>
 </nav>
 
