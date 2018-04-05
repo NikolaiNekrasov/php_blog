b682092aa40f21bbff1ac732a4471b35a777c6e3
Nick <nikolai.nekrasov@gmail.com>
2018-04-05 10:14:35 +0300

    added a block "jumbotron" to the layout of the main page

--- index.php
+++ index.php
@@ -69,7 +69,16 @@
 </div>
 
 <!--- Jumbotron -->
-
+<div class="container-fluid">
+    <div class="row jumbotron">
+        <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+            <p class="lead">TxtTxtTxtTxt  TxtTxtTxtTxtTxt TxtTxtTxtTxt  TxtTxtTxtTxt TxtTxtTxtTxt TxtTxtTxtTxt  TxtTxtTxtTxtTxt TxtTxtTxtTxt  TxtTxtTxtTxt TxtTxtTxtTxt</p>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+            <a href="#"><button type="button" class="btn btn-outline-secondary btn-lg">Web hosting</button></a>
+        </div>
+    </div>
+</div>
 
 <!--- Welcome Section -->
 
