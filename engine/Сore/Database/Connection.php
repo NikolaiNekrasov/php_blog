bcb4606b3e4c189e01fce0af7d83ef5cd8974744
NikolaiNekrasov <nikolai.nekrasov@gmail.com>
2018-04-09 16:38:46 +0400

    Services, ApstractProvider, Dependency injection - part 1

---
+++ engine/Сore/Database/Connection.php
@@ -0,0 +1,68 @@
+<?php 
+
+namespace Engine\Core\Database;
+
+class Connection {
+
+	private $link;
+
+	/**
+	* Database constructor
+	*/
+
+	public function __construct()
+	{
+		$this->connect();
+	}
+
+   /**
+	* @return $this
+	*/
+
+	private function connect()
+	{
+		$config = require_once 'config.php';
+
+		$dsn = 'mysql:host='.$config['host']';dbname='.$config['dbname']';charset='.$config['charset'];
+
+		this->link = new PDO($dsn, '.$config['username']', '.$config['password']');
+
+		return $this;
+
+	}
+
+	/**
+	* @return $sql
+	* @return mixed
+	*/
+
+	public function execute($sql)
+	{
+		$sth = $this->link->prepare($sql);
+
+		return $sth->execute();
+
+	}
+
+	/**
+	* @return $sql
+	* @return array
+	*/
+
+	public function query($sql)
+	{
+		$exe = $this->execute($sql);
+
+		result = $exe->fetchAll(PDO::FETCH_ASSOC);
+
+		if($result === false) {
+			return [];
+		}
+
+		return $result;
+
+	}
+
+}
+
+?>
