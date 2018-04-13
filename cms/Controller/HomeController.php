c6cff929f3679b78dfffff32992cf6578a846898
NikolaiNekrasov <nikolai.nekrasov@gmail.com>
2018-04-13 15:05:58 +0300

    Create a class view and add a new service. Display the first template on the render

--- cms/Controller/HomeController.php
+++ cms/Controller/HomeController.php
@@ -7,8 +7,9 @@ class HomeController extends CmsController
 
     public function index()
     {
-        echo 'Index Page';
-    }
+        $data = ['name' => 'Artem'];
+        $this->view->render('index', $data);
+     }
     public function news($id)
     {
         echo  $id;
