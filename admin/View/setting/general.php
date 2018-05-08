bd38c7d5c572772408bd47d46c005efb64070141
NikolaiNekrasov <nikolai.nekrasov@gmail.com>
2018-05-08 14:28:03 +0300

    Language, View, Settings Update

--- admin/View/setting/general.php
+++ admin/View/setting/general.php
@@ -11,26 +11,32 @@
             <div class="col">
                 <form>
                     <?php foreach($settings as $setting):?>
-                    <div class="form-group row">
-                        <label for="formNameSite" class="col-2 col-form-label">
-                            <?= $setting->name ?>
-                        </label>
-                        <div class="col-10">
-                            <input class="form-control" type="text" name="<?= $setting->get_field ?>" value="<?= $setting->value ?>" id="formNameSite">
-                        </div>
-                    </div>
-                    <?php endforeach; ?>
-                    <div class="form-group row">
-                        <label for="formLangSite" class="col-2 col-form-label">
-                            Language
-                        </label>
-                        <div class="col-10">
-                            <select class="form-control" id="formLangSite">
-                                <option value="english">English</option>
-                            </select>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Save changes</button>
+                        <?php if($setting->key_field == 'languages'): ?>
+                            <div class="form-group row">
+                                <label for="formNameSite" class="col-2 col-form-label">
+                                    <?= $setting->name ?>
+                                </label>
+                                <div class="col-10">
+                                    <select class="form-control" name="<?= $setting->key_field ?>" id=""
+                                </div>
+                                
+                                
+                                <div class="col-10">
+                                    <input class="form-control" type="text" name="<?= $setting->get_field ?>" value="<?= $setting->value ?>" id="formNameSite">
+                                </div>
+                            </div>
+                            <?php endforeach; ?>
+                            <div class="form-group row">
+                                <label for="formLangSite" class="col-2 col-form-label">
+                                    Language
+                                </label>
+                                <div class="col-10">
+                                    <select class="form-control" id="formLangSite">
+                                        <option value="english">English</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
                 </form>
             </div>
         </div>
